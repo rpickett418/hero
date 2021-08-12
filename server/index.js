5882e3c6ba4e91f08d3f5fac4ff8d43e46bb8661
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-12 10:14:02 -0600

    hero-up

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express')
 const path = require('path')
 
-const app = express();
+const app = express()
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '..index.html'))
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5050
 
 app.listen(port, () => {
-    console.log(`App listening on ${port}`)
-});
+  console.log(`Listening on port ${port}`)
+})
